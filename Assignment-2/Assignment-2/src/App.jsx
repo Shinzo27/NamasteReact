4f9e76ab81c1923c764519ed25af5c10bb66b0dc
Shinzo27 <20bmiit031@gmail.com>
2024-01-15 14:08:48 +0530

    Assignment-2

---
+++ Assignment-2/Assignment-2/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p style={{fontSize:"30px"}}>Hello Kid, Stay Motivated, You have to achieve a lot!</p>
+    </>
+  )
+}
+
+export default App
