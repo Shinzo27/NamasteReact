b05d77f8e1fb2be1ab8cf25819509254c4be8446
Shinzo27 <20bmiit031@gmail.com>
2024-02-12 11:54:53 +0530

    12-02

--- todoRedux/src/App.jsx
+++ todoRedux/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import AddTodo from './Components/AddTodo'
+import Todos from './Components/Todos'
 
 function App() {
 
   return (
     <>
-      
+      <h1>Todo List with Redux</h1>
+      <AddTodo/>
+      <Todos/>
     </>
   )
 }
--- todoRedux/src/Components/AddTodo.jsx
+++ todoRedux/src/Components/AddTodo.jsx
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
-
+import { useDispatch } from 'react-redux'
+import { addTodo } from '../Features/Todo/todoSlice'
 const AddTodo = () => {
-
+  
   const [ input, setInput ] = useState('')
+  const dispatch = useDispatch();
+  
+  const addTodoHandler = (e) => {
+    e.preventDefault();
+    dispatch(addTodo(input))
+    setInput('')
+  }
   return (
     <div>
       <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
