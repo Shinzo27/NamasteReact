5bdcc0a0978dd9375b1b8be388cbbdf7c789b46c
Shinzo27 <20bmiit031@gmail.com>
2024-02-27 23:18:50 +0530

    27-02

--- BlogSpot/src/appwrite/auth.js
+++ BlogSpot/src/appwrite/auth.js
@@ -1,2 +1,17 @@
 import config from "../config/config";
 
+import { Client, Account } from "appwrite";
+
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+    .setProject('<PROJECT_ID>');                 // Your project ID
+
+const account = new Account(client);
+
+const promise = account.createEmailPasswordSession('email@example.com', 'password');
+
+promise.then(function (response) {
+    console.log(response); // Success
+}, function (error) {
+    console.log(error); // Failure
+});
