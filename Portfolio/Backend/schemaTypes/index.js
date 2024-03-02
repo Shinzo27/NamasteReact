88add06a70a45a77c2ee0caf1951b5596afa7c2f
Shinzo27 <20bmiit031@gmail.com>
2024-03-02 19:33:32 +0530

    schemas

--- Portfolio/Backend/schemaTypes/index.js
+++ Portfolio/Backend/schemaTypes/index.js
@@ -1,1 +1,9 @@
-export const schemaTypes = []
+import testimonials from "./testimonials";
+import aboutus from "./aboutus";
+import brands from "./brands";
+import contact from "./contact";
+import experiences from "./experiences";
+import skills from "./skills";
+import works from "./works";
+
+export const schemaTypes = [ testimonials, aboutus, brands, contact, experiences, skills, works ];
