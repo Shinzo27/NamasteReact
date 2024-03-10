0f5413e1c54c07421659f4c20b9e4035e8c325c3
Shinzo27 <20bmiit031@gmail.com>
2024-03-10 23:55:25 +0530

    frontend

--- Portfolio/src/containers/About/About.jsx
+++ Portfolio/src/containers/About/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
 
   return (
     <>
-      <h2 className='head-text' style={{paddingTop: 40}}>I know that <span>Good Design</span><br/> means <span>Good Business</span>
+      <h2 className='head-text' style={{paddingTop: 40}}>I know that <span>Good Design</span><br/> means <span>Good Business!!</span>
       </h2>
 
       <div className='app__profiles'>
