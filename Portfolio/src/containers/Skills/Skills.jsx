cc99239ee4286f360c6c5e1f3521a9f41af02d90
Shinzo27 <20bmiit031@gmail.com>
2024-03-15 17:29:23 +0530

    frontend

--- Portfolio/src/containers/Skills/Skills.jsx
+++ Portfolio/src/containers/Skills/Skills.jsx
@@ -10,17 +10,24 @@ import './Skills.scss';
 const Skills = () => {
 
   const [Skills, setSkills] = useState([])
-  const [Experience, setExperience] = useState([])
+  const [Experiences, setExperience] = useState([])
 
   let PROJECT_ID = "egh2tbgp";
   let DATASET = "production";
-  let QUERY = '*[_type == "experiences"]';
+  let QUERY = '*[_type == "workExperience"]';
   let skillsQuery = '*[_type == "skills"]'
 
   let ExpURL = `https://${PROJECT_ID}.api.sanity.io/v2022-02-01/data/query/${DATASET}?query=${QUERY}`;
   let URL = `https://${PROJECT_ID}.api.sanity.io/v2022-02-01/data/query/${DATASET}?query=${skillsQuery}`;
 
   useEffect(() => {
+    
+    fetch(ExpURL)
+    .then((res) => res.json())
+    .then(({result}) => {
+      console.log(result);
+      setExperience(result)
+    })
 
     fetch(URL)
     .then((res) => res.json())
@@ -37,7 +44,7 @@ const Skills = () => {
       </h2>
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
-          {Skills.map((skill) => (
+          {Skills?.map((skill) => (
             <motion.div
               whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
@@ -54,33 +61,43 @@ const Skills = () => {
             </motion.div>
           ))}
         </motion.div>
-        {/* <motion.div
-          className='app__skills-exp'
-        >
-          {Experience.works.map((work)=>(
-            <>
-              <motion.div 
-              whileInView={{opacity: [0,1]}}
-              transition={{ duration: 0.5 }}
-              className='app__skills-exp-work'
-              data-tip
-              data-for={work.name}
-              key={work.name}
-              >
-                <h4 className='bold-text'>{work.name}</h4>
-                <p className='p-text'>{work.company}</p>
+        <div className="app__skills-exp">
+          {Experiences?.map((experience) => (
+            <motion.div
+              className="app__skills-exp-item"
+              key={experience.year}
+            >
+              <div className="app__skills-exp-year">
+                <p className="bold-text">{experience.year}</p>
+              </div>
+              <motion.div className="app__skills-exp-works">
+                {experience?.works?.map((work) => (
+                  <>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className="app__skills-exp-work"
+                      data-tip
+                      data-for={work.name}
+                      key={work.name}
+                    >
+                      <h4 className="bold-text">{work.name}</h4>
+                      <p className="p-text">{work.company}</p>
+                    </motion.div>
+                    <ReactTooltip
+                      id={work.name}
+                      effect="solid"
+                      arrowColor="#fff"
+                      className="skills-tooltip"
+                    >
+                      {work.desc}
+                    </ReactTooltip>
+                  </>
+                ))}
               </motion.div>
-              <ReactTooltip
-                id={work.name}
-                effect="solid"
-                arrowColor="#fff"
-                className='skills-tooltip'
-              >
-                {work.desc}
-              </ReactTooltip>
-            </>
+            </motion.div>
           ))}
-        </motion.div> */}
+        </div>
       </div>
     </>
   )
